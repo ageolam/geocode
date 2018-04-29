8d057a5114259c6246f72e2ef99370b37ed31e4a
Andreas Georgiou <ageolam@gmail.com>
2018-04-29 21:13:13 +0300

    added php script to geocode

---
+++ index.php
@@ -0,0 +1,37 @@
+<?php
+require 'vendor/autoload.php';
+
+$dotenv = new Dotenv\Dotenv(__DIR__);
+$dotenv->load();
+$apiKey = getenv('GEOCODE_API');
+$countriesStates = require('germanyStates.php');
+$locationArray   = [];
+
+foreach ($countriesStates as $country => $stateArray) {
+    foreach ($stateArray as $state) {
+        $location = getGeocode($state);
+        $locationArray[$country][$state] = [
+            'lat'  => $location['lat'],
+            'lon'  => $location['lng'],
+        ];
+    }
+}
+
+var_export($locationArray);
+
+
+
+function getGeocode($state)
+{
+    $state = str_replace(' ', '%20', $state);
+
+    $curl = curl_init();
+    curl_setopt_array($curl, [
+        CURLOPT_RETURNTRANSFER => 1,
+        CURLOPT_URL => 'https://maps.googleapis.com/maps/api/geocode/json?address='.$state.'&key=AIzaSyDQYtTn7H1sUioyRERVAue4h_zSlLbIpyw',
+    ]);
+    $resp = json_decode(curl_exec($curl), true);
+    curl_close($curl);
+
+    return $resp['results'][0]['geometry']['location'];
+}
